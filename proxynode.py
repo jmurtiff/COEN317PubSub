4845afe71dab8284156f42f02ea97532b506ba48
Jordan <jordanmurtiff@gmail.com>
2022-12-02 21:53:36 -0800

    Removed more log messages

--- proxynode.py
+++ proxynode.py
@@ -294,9 +294,6 @@ def receiverthread():
               verified = verify(decrypted_message, signature, pub_publicKey)
               # once decryption and verification is done
               if verified and decrypted_message:
-                #log("VERIFIED MESSAGE, SENDING MESSAGE TO SUBSCRIBER")
-                print(decoded_data.keys())
-                print(decoded_data)
                 send_message_to_subscribers(decrypted_message, decoded_data["Subscribers"])
               else:
                 log("FAILED VERIFIED, DROPPING THE MESSAGE")
